806b13541eab264b46add56835c539c2ae28bec1
Arun Regmi <aregmi450@gmail.com>
2020-12-17 20:04:33 +0545

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.red),
       // routing different pages from one place to another
-      initialRoute: 'Login',
+      initialRoute: 'SignIn',
       // class name of every other page on stateless widget is given
       // routing to every other page when class is called
       routes: {
